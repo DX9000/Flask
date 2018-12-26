98cc9e4b41c945047a86882de3b6400fb165ebd2
DX9000 <501980665@qq.com>
2018-12-26 15:37:52 +0800

    数据库配置

--- manage.py
+++ manage.py
@@ -1,8 +1,22 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 
 class Config():
     DEBUG = True
 
+    SQALCHEMY_DATABASES_URL = 'mysql:root@172.0.0.1:3306/infomation27'
+    SQLALCHEMY_TRACK_MODIFICATIONS = Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
+
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return '00'
+
+if __name__ == '__main__':
+    app.run()
